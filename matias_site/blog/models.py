1ff5d0705dcd5f545c9060ff54faf142090c8430
Matias Cardenas <cardenasmatias.1990@gmail.com>
2022-09-21 22:44:38 +0200

    feat: adding Post model plus adding isort, black and flake8

--- matias_site/blog/models.py
+++ matias_site/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        "auth.User"
+    )  # linking the user as author, as website is only intended to be used by me
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self) -> None:
+        """When I hit the 'publish' button I will set the publish date to now()"""
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def __str__(self):
+        return self.title

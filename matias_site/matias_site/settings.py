34d852df62a73459957adab0cbe63fa4473c2c4a
Matias Cardenas <cardenasmatias.1990@gmail.com>
2022-10-29 13:31:54 +0200

    chore: increasing data upload memory size

--- matias_site/matias_site/settings.py
+++ matias_site/matias_site/settings.py
@@ -142,3 +142,5 @@ LOGIN_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 20971520

f12f61078c4a330048f1d854f3e715d6757cc77c
Matias Cardenas <matias.cardenas@sendcloud.com>
2023-12-21 19:51:22 +0100

    chore: running with django server for now

--- start_server.sh
+++ start_server.sh
@@ -2,5 +2,6 @@ yes | poetry run python /usr/matias_django_blog/matias_site/manage.py makemigrat
 yes | poetry run python /usr/matias_django_blog/matias_site/manage.py migrate
 yes | poetry run python /usr/matias_django_blog/matias_site/manage.py collectstatic --noinput
 cd matias_site/
-poetry run gunicorn matias_site.wsgi --bind 0.0.0.0:8888 --workers 3 --daemon
-nginx -g "daemon off;"
+poetry run python /usr/matias_django_blog/matias_site/manage.py runserver 0.0.0.0:8888
+#poetry run gunicorn matias_site.wsgi --bind 0.0.0.0:8888 --workers 3 --daemon
+#nginx -g "daemon off;"

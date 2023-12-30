da1d1adda4250e7abec74e0307857b6a982de9b7
Matias Cardenas <cardenasmatias.1990@gmail.com>
2023-12-31 00:48:14 +0100

    chore: removing nginx configuration, as it nows lies on the server

--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,3 @@ yes | poetry run python /usr/matias_django_blog/matias_site/manage.py migrate
 yes | poetry run python /usr/matias_django_blog/matias_site/manage.py collectstatic --noinput
 cd matias_site/
 poetry run -c gunicorn_config.py gunicorn matias_site.wsgi
-nginx -g "daemon off;"
